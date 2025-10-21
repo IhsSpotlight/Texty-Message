d56b211d80140244a959552c3621d97f796d9786
sharioul <sharioul.sagar@gmail.com>
2025-10-21 11:16:47 +0600

    Sharedpreference module

--- app/src/main/java/org/freedu/sharedpreferenceb6/MainActivity.kt
+++ app/src/main/java/org/freedu/sharedpreferenceb6/MainActivity.kt
@@ -3,6 +3,7 @@ package org.freedu.sharedpreferenceb6
 import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,29 +12,35 @@ import androidx.core.view.WindowInsetsCompat
 import org.freedu.sharedpreferenceb6.databinding.ActivityMainBinding
 import androidx.core.content.edit
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
     private lateinit var binding: ActivityMainBinding
-    private val PREFS_NAME = "my_prefs"
-    private val KEY_NAME = "key_name"
+    private  val PREFS_NAME = "my_prefs"
+    private  val KEY_NAME = "key_name"
+    private  val KEY_NAME2 = "key_name2"
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         binding.saveBtn.setOnClickListener {
             val name = binding.nameET.text.toString().trim()
-            if (name.isNotEmpty()) {
+            val name2 = binding.nameETtwo.text.toString().trim()
+            if (name.isNotEmpty() && name2.isNotEmpty())
+            {
                 val prefs = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
-                prefs.edit { putString(KEY_NAME, name) }
-                    val savedName = prefs.getString(KEY_NAME, null)
-                    binding.savedNameTV.text =  "Saved: ${savedName ?: "(none)"}"
-
+                prefs.edit {
+                    putString(KEY_NAME, name)
+                    putString(KEY_NAME2, name2)
+                }
+                val saveName = prefs.getString(KEY_NAME,null)
+                val saveName2 = prefs.getString(KEY_NAME2,null)
+                binding.savedNameTV.text = "Saved: $saveName $saveName2 "
             }
         }
+
     }
 
 
